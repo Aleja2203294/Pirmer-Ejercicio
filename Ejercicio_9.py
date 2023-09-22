7b24887499a92112085c0202a3c489b1ee643852
Aleja2203294 <144927903+Aleja2203294@users.noreply.github.com>
2023-09-22 10:35:00 -0500

    Add files via upload

---
+++ Ejercicio_9.py
@@ -0,0 +1,43 @@
+#Vamos a realizar un programa que realice una tupla compuesta por diferentes 
+#tipos de datos que incluyan variables que estén orientados a realizar diferentes 
+#acciones enfocadas en generar resultados en esas variaciones
+
+#print ("Este es un programa de tuplas")
+
+#n = [(1,4),(4,8)]
+#t = [("Alejandra",1),(34,"gato")]
+
+#print (n,t)
+
+res = ()
+
+#for n in res:
+    #print (n)
+    #if n == res:
+        #print (res)
+
+#Voy a localizar dentro de una tupla la posición determinada por un operador 
+# de referencia como el :.
+
+#A = ("casa", "Programa para posicionar tuplas en una variable", [2,9,2022]) 
+
+#print (A[2:3])
+
+#for A in True: 
+    #print (A)
+    #if A == True: 
+        #print ("casa")
+
+#[2:4][3:9]
+B = ("árbol", "flor", "sol" [3,6], "carro", [9,12], "perro")
+
+print (B[0:1])
+print (B[2:4])
+print (B[3:9])
+ 
+for B in res: 
+    print (B)
+    if B == res: 
+        print (B)
+
+
